053016c0ed83f00933156f525bdab5a38275ed1f
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2024-11-25 14:37:35 +0100

    Create exec_jenkins_funciona.groovy
    Prueba de llamada a Jenkins desde JIRA

---
+++ exec_jenkins_funciona.groovy
@@ -0,0 +1,28 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+def jenkinsJobTrigger() {
+    def jenkinsUrl = "http://srvcceacml77:8080/job/SIACUDEF_AP/job/develop/build"
+    def jenkinsUser = "lmunma1"
+    def jenkinsToken = "11253d0293d61aa417fef153683d06e99e"
+
+    def command = [
+        "curl", "-X", "POST",
+        "-u", "${jenkinsUser}:${jenkinsToken}",
+        jenkinsUrl
+    ]
+
+    def process = command.execute()
+    def output = process.text
+    def exitCode = process.waitFor()
+
+    if (exitCode == 0) {
+        log.info("Jenkins job triggered successfully!")
+        log.info("Response: ${output}")
+    } else {
+        log.error("Failed to trigger Jenkins job.")
+        log.error("Response: ${output}")
+    }
+}
+
+jenkinsJobTrigger()
